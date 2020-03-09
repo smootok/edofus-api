4a8b204032b40d4dff1e3b69db06c9cc8a9aa99e
smootok <smootok@gmail.com>
2020-03-09 22:58:30 +0100

    Serve static files in public directory and allow origin

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  next()
+})
+
+app.use(express.static('public'))
 app.use(express.json())
 
 app.use('/api/v1/encyclopedia', encyclopediaRouter)
